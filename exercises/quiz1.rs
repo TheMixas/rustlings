89b8d37de98a08509a9c3d897d2560b3567ab933
TheMixas <themixaslt@gmail.com>
2024-02-29 15:35:29 +0000

    Progress

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount:i16) -> i16 {
+    let mut apple_cost :i16 = 2;
+    if amount > 40{
+        apple_cost = 1;
+    }
+    apple_cost * amount
+
+}
 
 // Don't modify this function!
 #[test]
